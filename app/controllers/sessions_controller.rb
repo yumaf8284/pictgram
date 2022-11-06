202bf1e2be2d9787d4be74812566e8f7e97dd5fa
Y.fujita <yuma.yf425@gmail.com>
2022-11-06 13:41:01 +0000

    kadai2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    # binding.pry
+    user = User.find_by(email_params)
+    if user && user.authenticate(password_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -28,7 +29,12 @@ private
    @current_user = nil
  end
  
- def user_params
-  params.require(:user).permit(:email, :password)
+ def email_params
+  params.require(:session).permit(:email)
  end
+ 
+ def password_params
+  params.require(:session).permit(:password)
+ end
+ 
 end
