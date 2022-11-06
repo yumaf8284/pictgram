bad11d78d0cfecbac7daaeb0245cb23310b8f188
Y.fujita <yuma.yf425@gmail.com>
2022-11-06 14:18:40 +0000

    syuusei

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     # binding.pry
-    user = User.find_by(email_params)
+    user = User.find_by(email_params[:email])
     if user && user.authenticate(password_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
