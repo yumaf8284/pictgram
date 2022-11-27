892bcbc8af5e51ac5f9bcd1df000694c83dee773
Y.fujita <yuma.yf425@gmail.com>
2022-11-27 06:55:01 +0000

    kadai6

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,4 +7,7 @@ class Topic < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   resources :users
   resources :topics
   
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
   get     '/login',  to: 'sessions#new'
   post    '/login',  to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
